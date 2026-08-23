716fca26aba7519d3e072587fe6a121db5b16203
haruka <uniiiisuke@gmail.com>
2026-08-23 18:01:15 +0900

    feat:お気に入り一覧・追加削除の実装とフロントエンドのセットアップ周り

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\FavoriteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,5 +34,12 @@ Route::middleware('auth')->group(function () {
         ->only(['edit', 'update', 'destroy']);
 
     Route::post('/books/{book}/reviews', [ReviewController::class, 'store'])->name('reviews.store');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::resource('favorites', FavoriteController::class)
+        ->only(['index']);
+
+    Route::post('/books/{book}/favorites', [FavoriteController::class, 'toggle'])->name('favorites.toggle');
 });
 
