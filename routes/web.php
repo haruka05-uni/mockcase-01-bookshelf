39118635e6eae8c064cd44b4bf818170099925a0
haruka <uniiiisuke@gmail.com>
2026-08-26 11:49:41 +0900

    fix: リダイレクト先を書籍一覧に変更

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,8 @@ use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\RankingController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+Route::redirect('/', '/books');
 
 Route::middleware('auth')->group(function () {
     //書籍関連
