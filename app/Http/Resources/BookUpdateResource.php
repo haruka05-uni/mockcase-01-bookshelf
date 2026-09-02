d2b8df778837fa0afda3d38e42960dcb981de897
haruka <uniiiisuke@gmail.com>
2026-09-02 10:55:23 +0900

    fix: APIレスポンス形式を修正

--- app/Http/Resources/BookUpdateResource.php
+++ app/Http/Resources/BookUpdateResource.php
@@ -5,11 +5,12 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class BookStoreResource extends JsonResource
+class BookUpdateResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'author' => $this->author,
             'isbn' => $this->isbn,
